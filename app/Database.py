f8129a914fc50ff866818d6090b22c2d3d6b8072
Rakshit Bhardwaj <rakshitbhardwaj298@gmail.com>
2026-07-04 22:06:03 +0530

    fix: load DATABASE_URL from environment variables

--- app/Database.py
+++ app/Database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:%40djokernole@localhost/jobs"
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal= sessionmaker(bind=engine) 
 
